5ee8e092203722dabfa3e01414f19478322f25a4
Leighton Coutinho <coutinho.leighton1@gmail.com>
2023-11-28 23:47:20 -0500

    added ignores

--- back/config/db.js
+++ back/config/db.js
@@ -1,3 +1,24 @@
+require('dotenv').config(); // Load environment variables from .env
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(process.env.mongoURI, {
+      useNewUrlParser: true,
+    });
+
+    console.log('MongoDB is Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
+/*
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
@@ -16,4 +37,4 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+module.exports = connectDB; */
